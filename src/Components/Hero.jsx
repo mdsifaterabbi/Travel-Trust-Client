f226fe1f216d3249f4f483b25f4e1cabf1709d61
mdsifaterabbi <mdsifaterabbi641@gmail.com>
2024-03-22 18:05:57 +0600

    working on slider texts and drop down

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,6 +4,7 @@ import "slick-carousel/slick/slick-theme.css";
 import styles from "./Arrows.module.css";
 import { BsArrowLeft } from "react-icons/bs";
 import { BsArrowRight } from "react-icons/bs";
+import { useForm } from "react-hook-form";
 
 function SampleNextArrow(props) {
   const { className, style, onClick } = props;
@@ -35,6 +36,39 @@ const Hero = () => {
     nextArrow: <SampleNextArrow />,
     prevArrow: <SamplePrevArrow />,
   };
+  const heroSliderContents = [
+    {
+      id: 1,
+      heading: "Visa & Passport Agency 1",
+      subTitle:
+        "Don’t let a little thing like paperwork stop you from enjoying your travels",
+    },
+    {
+      id: 2,
+      heading: "Visa & Passport Agency 2",
+      subTitle:
+        "Don’t let a little thing like paperwork stop you from enjoying your travels",
+    },
+    {
+      id: 3,
+      heading: "Visa & Passport Agency 3",
+      subTitle:
+        "Don’t let a little thing like paperwork stop you from enjoying your travels",
+    },
+    {
+      id: 4,
+      heading: "Visa & Passport Agency 4",
+      subTitle:
+        "Don’t let a little thing like paperwork stop you from enjoying your travels",
+    },
+  ];
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
 
   return (
     <>
@@ -43,18 +77,44 @@ const Hero = () => {
           className={`slider-container w-[100vw] md:w-[98vw] lg:w-[98vw] xl:w-[98vw] mx-auto`}
         >
           <Slider {...settings}>
-            <div className="bg-sky-500 py-[100px]">
-              <h1 className="text-black text-center text-[40px]">Image 1</h1>
-            </div>
-            <div className="bg-purple-500 py-[100px]">
-              <h1 className="text-black text-center text-[40px]">Image 2</h1>
-            </div>
-            <div className="bg-orange-500 py-[100px]">
-              <h1 className="text-black text-center text-[40px]">Image 3</h1>
-            </div>
-            <div className="bg-pink-500 py-[100px]">
-              <h1 className="text-black text-center text-[40px]">Image 4</h1>
-            </div>
+            {heroSliderContents.map((s, index) => (
+              <div key={index} className="border pt-[130px] pb-[90px]">
+                <div className="w-[50vw] mx-auto">
+                  <h1 className="text-black text-left xl:text-[40px]">
+                    {s.heading}
+                  </h1>
+                  <p className="text-left xl:text-[22px]">{s.subTitle}</p>
+                  <div className="bg-purple-600 w-[50%] py-[15px] rounded-md">
+                    <form onSubmit={handleSubmit((data) => console.log(data))}>
+                      {/* <label htmlFor="serviceOrDestination">
+                        Choose service or destination:
+                      </label> */}
+                      <select
+                        {...register("serviceOrDestination", {
+                          required: true,
+                        })}
+                        id="serviceOrDestination"
+                        className="mx-[30px]"
+                      >
+                        <option value="">
+                          -- Choose service or destination --
+                        </option>{" "}
+                        {/* Default option */}
+                        <option value="service1">Service 1</option>
+                        <option value="service2">Service 2</option>
+                        <option value="destination1">Destination 1</option>
+                        <option value="destination2">Destination 2</option>
+                      </select>
+                      {errors.serviceOrDestination && (
+                        <p>Please select a service or destination.</p>
+                      )}
+
+                      <input type="submit" />
+                    </form>
+                  </div>
+                </div>
+              </div>
+            ))}
           </Slider>
         </div>
       </div>
